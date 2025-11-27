0f0fa32df211b102a56b8d83ff4543785b752e16
Lea Magnavita Santos <lea-marialva.magnavitasantos@iu-study.org>
2025-11-27 12:13:58 +0100

    added booking confirmation with hidden CSS, JS submit message

---
+++ main.js
@@ -0,0 +1,5 @@
+//hier wird das Absenden des Formulars abgefangen, um eine Buchungsbestätigung zu simulieren, ohne die Seite neu zu laden
+document.forms[0] .addEventListener("submit", function(event){event.preventDefault()
+let buchungElement = document.getElementById("buchung")
+buchungElement.classList.add("sichtbar")
+});

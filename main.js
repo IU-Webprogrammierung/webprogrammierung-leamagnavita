a7be6e422f00796d580f39f9e7e18bbc35070a5d
Lea Magnavita Santos <lea-marialva.magnavitasantos@iu-study.org>
2025-11-27 15:55:02 +0100

    hid form content when form is submitted

--- main.js
+++ main.js
@@ -2,4 +2,7 @@
 document.forms[0] .addEventListener("submit", function(event){event.preventDefault()
 let buchungElement = document.getElementById("buchung")
 buchungElement.classList.add("sichtbar")
+//Formularinhalt für Buchungsbestätigung ausblenden
+let verstecktElement = document.querySelector(".formularinhalt")
+verstecktElement.classList.add("versteckt")
 });

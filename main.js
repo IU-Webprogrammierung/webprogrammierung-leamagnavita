422ea4fbe0c0b1d6ba09e79838583b4a477d822c
Lea Magnavita Santos <lea-marialva.magnavitasantos@iu-study.org>
2025-12-02 12:46:31 +0100

    submit only allowed if checkbox is checked or number is selected, show alert otherwise

--- main.js
+++ main.js
@@ -1,8 +1,36 @@
 //hier wird das Absenden des Formulars abgefangen, um eine Buchungsbestätigung zu simulieren, ohne die Seite neu zu laden
-document.forms[0] .addEventListener("submit", function(event){event.preventDefault()
-let buchungElement = document.getElementById("buchung")
-buchungElement.classList.add("sichtbar")
-//Formularinhalt für Buchungsbestätigung ausblenden
-let verstecktElement = document.querySelector(".formularinhalt")
-verstecktElement.classList.add("versteckt")
+document.forms[0].addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let eingabe = document.getElementById("checkbox").checked;
+    let zahl = document.getElementById("number").value;
+    //checkbox ist angeklickt = absenden möglich oder Zahlenfeld enthält Wert = absenden möglich, ansonsten Fehlermeldung
+    if (eingabe) {
+        //Formularinhalt für Buchungsbestätigung ausblenden
+        let verstecktElement = document.querySelector(".formularinhalt");
+        verstecktElement.classList.add("versteckt");
+        let buchungElement = document.getElementById("buchung");
+        buchungElement.classList.add("sichtbar");
+    }
+    else if (zahl !== '') {
+        let verstecktElement = document.querySelector(".formularinhalt");
+        verstecktElement.classList.add("versteckt");
+        let buchungElement = document.getElementById("buchung");
+        buchungElement.classList.add("sichtbar");
+    }
+    else {
+        alert("Bitte Bedarf angeben");
+    }
 });
+
+
+
+
+
+
+
+
+
+
+
+

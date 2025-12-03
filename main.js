96a844ea2b5f30b2d6af66ea9534ac88ec778add
Lea Magnavita Santos <lea-marialva.magnavitasantos@iu-study.org>
2025-12-03 13:17:45 +0100

    added Button to reset form after submit

--- main.js
+++ main.js
@@ -5,25 +5,31 @@ document.forms[0].addEventListener("submit", function (event) {
     let eingabe = document.getElementById("checkbox").checked;
     let zahl = document.getElementById("number").value;
     //checkbox ist angeklickt = absenden möglich oder Zahlenfeld enthält Wert = absenden möglich, ansonsten Fehlermeldung
-    if (eingabe) {
+    if (eingabe || zahl !== '') {
         //Formularinhalt für Buchungsbestätigung ausblenden
         let verstecktElement = document.querySelector(".formularinhalt");
         verstecktElement.classList.add("versteckt");
-        let buchungElement = document.getElementById("buchung");
+        let buchungElement = document.querySelector(".buchungsbestätigung");
         buchungElement.classList.add("sichtbar");
+        //Button für Wiederherstellung des Formulars einblenden
+        let neuButton = document.getElementById("neu");
+        neuButton.classList.add("sichtbar");
     }
-    else if (zahl !== '') {
-        let verstecktElement = document.querySelector(".formularinhalt");
-        verstecktElement.classList.add("versteckt");
-        let buchungElement = document.getElementById("buchung");
-        buchungElement.classList.add("sichtbar");
-    }
+    //Fehlermeldung
     else {
         alert("Bitte Bedarf angeben");
-    }
-});
+    }});
+//Button, um nach Absenden das Formular wieder einzublenden
+let neuButton = document.getElementById("neu");
+neuButton.addEventListener("click", function () {
+    let verstecktElement = document.querySelector(".formularinhalt");
+    verstecktElement.classList.remove("versteckt");
 
+    let buchungElement = document.querySelector(".buchungsbestätigung");
+    buchungElement.classList.remove("sichtbar");
 
+    let neuButton = document.getElementById("neu");
+    neuButton.classList.remove("sichtbar");});
 
 
 

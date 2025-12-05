534da7852bcb983512d207d18e538ce153ac7e66
Lea Magnavita Santos <lea-marialva.magnavitasantos@iu-study.org>
2025-12-05 14:54:42 +0100

    fixed overflow

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //Text auf Startseite automatisch eingeklappt unter 768px
 let closeAnschreiben = document.getElementById("text");
 let bildschirmBreite = window.innerWidth;
+
 if (bildschirmBreite < 768) {
     closeAnschreiben.removeAttribute("open");
 }
@@ -11,25 +12,32 @@ document.forms[0].addEventListener("submit", function (event) {
 
     let eingabe = document.getElementById("checkbox").checked;
     let zahl = document.getElementById("number").value;
+    
     //checkbox ist angeklickt = absenden möglich oder Zahlenfeld enthält Wert = absenden möglich, ansonsten Fehlermeldung
     if (eingabe || zahl !== '') {
+
         //Formularinhalt für Buchungsbestätigung ausblenden
         let verstecktElement = document.querySelector(".formularinhalt");
         verstecktElement.classList.add("versteckt");
+
         //Buchungsbestätigung einbelenden
         let buchungElement = document.querySelector(".buchungsbestätigung");
         buchungElement.classList.add("sichtbar");
+
         //Buchungs-icon einblenden
         let buchungIcon = document.getElementById("buchungicon");
         buchungIcon.classList.add("sichtbar");
+
         //Button für Wiederherstellung des Formulars einblenden
         let neuButton = document.getElementById("neu");
         neuButton.classList.add("sichtbar");
     }
+
     //Fehlermeldung
     else {
         alert("Bitte Bedarf angeben");
-    }});
+    }})
+
 //Button, um nach Absenden das Formular wieder einzublenden
 let neuButton = document.getElementById("neu");
 neuButton.addEventListener("click", function () {
